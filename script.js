6a99debc773d5ace0ad453260e2196336363e3b2
Ravi-Kumar-Yedla <136640114+Ravi-Kumar-Yedla@users.noreply.github.com>
2024-02-28 10:20:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 function highlight() {
     //Write your code here
-
+   let strong = document.querySelectorAll('strong');
+	for(let highlt of strong){
+		highlt.style.color = 'green' 
+	}
 
 }
 
 
 function return_normal() {
     //Write your code here
+	let strong = document.querySelectorAll('strong');
+	for(let highlt of strong){
+		highlt.style.color = 'black';
+	}
 
     
 }
